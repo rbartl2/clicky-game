5f6825c3afb33bf790722d74d348d021a294acc8
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-12-11 14:24:00 -0600

    app.js progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards,
+    clickedArray : [],
+    score: 0,
+    topScore: 0,
+    message: ""
+  };
+
+  shuffleArray = (picArray) => {
+    for (let i = picArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [picArray[i], picArray[j]] = [picArray[j], picArray[i]];
+    }
+    return picArray;
 }
+
+clickPicture = id => {
+  // Arrange the pictures in a random manner
+  const shuffledArray = this.shuffleArray(cards);
+  this.setState({cards: shuffledArray});
+  // if clicked an image already clicked set this.state.score = 0; empty clickeadArray, end of if block
+  if (this.state.clickedArray.includes(id)) {
+    this.setState({ score: 0, clickedArray: [], message: "Incorrect! Game Over. Click an image to start again!"});
+  }
+  else {
+    this.setState({
+      clickedArray: this.state.clickedArray.concat([id]),
+      score: this.state.score + 1,
+      message: "Correct!",
+    });
+  }
+  // set topscore = score if score>topscore.
+  if (this.state.score > this.state.topScore) {
+    this.setState({ topScore: this.state.score });
+  }
+  
+}
+}
+
+
+
+
+
 
 export default App;
