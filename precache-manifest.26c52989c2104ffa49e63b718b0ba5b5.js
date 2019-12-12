0a1d06cc52f14fc4be57644232ae8bc902c72b6b
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-12-11 18:38:42 -0600

    Updates

---
+++ precache-manifest.26c52989c2104ffa49e63b718b0ba5b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347c37b7e0a235d4c0bf3131647bf112",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b6beb572ee333054d89e",
+    "url": "/clicky-game/static/css/main.b326148b.chunk.css"
+  },
+  {
+    "revision": "00d61d0bbab4af0d0cec",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6beb572ee333054d89e",
+    "url": "/clicky-game/static/js/main.a218092e.chunk.js"
+  },
+  {
+    "revision": "b859e8c835d6a54c0ef8",
+    "url": "/clicky-game/static/js/runtime-main.bf081868.js"
+  }
+]);
